9f8632b712f361ff1f8ee742d61ed40855b1a2a3
Aurélien Wailly <aurelien.wailly@gmail.com>
2015-08-19 16:34:49 +0200

    Add ansible-cis task run

--- flower/hardening/tasks.py
+++ flower/hardening/tasks.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from subprocess import call
 
 app = Celery('tasks')
 app.config_from_object('celeryconfig')
@@ -6,3 +7,9 @@ app.config_from_object('celeryconfig')
 @app.task
 def add(x, y):
     return x + y
+
+@app.task
+def hardening_ex(user, ip, tag):
+    command = 'ansible-playbook -e "pipelining=True" -b -u %s --private-key=~/.ssh/id_rsa -i "%s," -t %s playbook.yml' % (user, ip, tag)
+    retcode = call(command.split(" "))
+    return retcode
