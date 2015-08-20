89894fec2f7ef5c56d240725bf81427ec619451d
Aurélien Wailly <aurelien.wailly@gmail.com>
2015-08-20 16:58:54 +0200

    Better error handling for SSH fingerprint

--- flower/hardening/tasks.py
+++ flower/hardening/tasks.py
@@ -12,10 +12,28 @@ def add(x, y):
     return x + y
 
 def trust_host(ip):
+    found = 0
+    with open("/home/ubuntu/.ssh/known_hosts", "r") as myfile:
+        lines = myfile.readlines()
+        for line in lines:
+            if ip in line:
+                found=1
+
+    if found:
+        print("Already trusted IP")
+        return 0
+
     p = subprocess.Popen(["ssh-keyscan", ip], stdout=subprocess.PIPE)
     output = p.communicate()[0]
-    key = output.split("\n")[-1]
-    #print("Found key: %s" % key)
+    key = output
+
+    if "ssh" not in key:
+        return 1
+
+    with open("/home/ubuntu/.ssh/known_hosts", "a") as myfile:
+        print("Writing key to known_hosts")
+        myfile.write(key)
+
     return p.returncode
 
 def get_credentials(vmid):
@@ -26,7 +44,6 @@ def get_credentials(vmid):
 def hardening_ex(vmid, ip, tag):
     result = {}
     result['id'] = str(uuid.uuid4())
-    result['error'] = ''
     status = trust_host(ip)
 
     if status != 0:
@@ -37,30 +54,35 @@ def hardening_ex(vmid, ip, tag):
     user, key = get_credentials(vmid)
     playbook = "/home/ubuntu/ansible/roles-ubuntu/playbook.yml"
 
-    #print("%s %s %s" % (repr(user), repr(ip), repr(tag)))
-    command = 'ansible-playbook -e "pipelining=True" -b -u %s --private-key=%s -i %s -t %s %s' % (user, key, ip, tag, playbook)
-    #print(repr(command.split(" ")))
+    print("%s %s %s" % (repr(user), repr(ip), repr(tag)))
+    command = 'ansible-playbook -e "pipelining=True" -b -u %s --private-key=%s -i %s, -t %s %s' % (user, key, ip, tag, playbook)
+    print(repr(command.split(" ")))
 
     p = subprocess.Popen(command.split(" "), stdout=subprocess.PIPE)
     output = p.communicate()[0]
-    #print(output)
+    print(output)
 
     result = {}
     result['returncode'] = p.returncode
-    result['id'] = str(uuid.uuid4())
 
-    date = datetime.datetime.now().isoformat()
-    formatted_audit = {}
+    try:
+        date = datetime.datetime.now().isoformat()
+        formatted_audit = {}
 
-    r = redis.Redis('localhost')
-    r.hset("audit_%s" % vmid, "date", date)
+        r = redis.Redis('localhost')
+        r.hset("audit:%s" % vmid, "date", date)
 
-    # Removing the first with useless information
-    tasks = output.split("TASK:")[1:]
-    for task in tasks:
-        audit_key = task.split("]")[0].split("[")[1]
-        audit_value = task.split("\n")[1:]
-        r.hset("audit_%s" % vmid, audit_key, audit_value)
+        score = output.split("PLAY RECAP")[1].split(":")[1].split("\n")[0]
+        r.hset("audit:%s:evolution" % vmid, date, score)
+
+        # Removing the first with useless information
+        tasks = output.split("TASK:")[1:]
+        for task in tasks:
+            audit_key = task.split("]")[0].split("[")[1]
+            audit_value = task.split("\n")[1:]
+            r.hset("audit_%s" % vmid, audit_key, audit_value)
+    except:
+        result['error'] = output
 
     #print(r.hgetall("audit_%s" % vmid))
 
