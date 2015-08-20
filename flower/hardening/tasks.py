c014dedbfca03c704e548301c2c67b514a81588f
Aurélien Wailly <aurelien.wailly@gmail.com>
2015-08-20 10:31:46 +0200

    Extend hardening with CIS and API

--- flower/hardening/tasks.py
+++ flower/hardening/tasks.py
@@ -1,5 +1,6 @@
 from celery import Celery
-from subprocess import call
+import subprocess
+import uuid
 
 app = Celery('tasks')
 app.config_from_object('celeryconfig')
@@ -8,8 +9,33 @@ app.config_from_object('celeryconfig')
 def add(x, y):
     return x + y
 
+def trust_host(ip):
+    p = subprocess.Popen(["ssh-keyscan", ip], stdout=subprocess.PIPE)
+    output = p.communicate()[0]
+    key = output.split("\n")[-1]
+    print("Found key: %s" % key)
+    return p.returncode
+
+def get_credentials(vmid):
+    priv_key = "~/.ssh/id_rsa"
+    return ("ubuntu", priv_key)
+
 @app.task
-def hardening_ex(user, ip, tag):
-    command = 'ansible-playbook -e "pipelining=True" -b -u %s --private-key=~/.ssh/id_rsa -i "%s," -t %s playbook.yml' % (user, ip, tag)
-    retcode = call(command.split(" "))
-    return retcode
+def hardening_ex(vmid, ip, tag):
+    trust_host(ip)
+    user,key = get_credentials(vmid)
+    playbook = "/home/ubuntu/ansible/roles-ubuntu/playbook.yml"
+
+    print("%s %s %s" % (repr(user), repr(ip), repr(tag)))
+    command = 'ansible-playbook -e "pipelining=True" -b -u %s --private-key=%s -i %s -t %s %s' % (user, key, ip, tag, playbook)
+    print(repr(command.split(" ")))
+
+    p = subprocess.Popen(command.split(" "), stdout=subprocess.PIPE)
+    output = p.communicate()[0]
+    print(output)
+
+    result = {}
+    result['returncode'] = returncode
+    result['id'] = uuid.uuid4()
+
+    return p.returncode
